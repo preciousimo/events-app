f00262e1c9445521023f7b0c68ea326bf77731bd
preciousimo <preciousimoniakemu@gmail.com>
2023-08-14 21:58:03 +0100

    Style card

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,13 +1,15 @@
 import { useNavigation } from "@react-navigation/native";
-import { Text, StyleSheet, TouchableOpacity, Image} from "react-native";
+import { Text, StyleSheet, TouchableOpacity, View, Image} from "react-native";
 
 export default function EventItem({id, name, description, qrCode}) {
     const navigation = useNavigation()
 
     return ( 
         <TouchableOpacity style={styles.card} onPress={()=>navigation.navigate("Event", {eventId: id, name, description})}>
-            <Text>{name}</Text>
-            <Text>{description}</Text>
+            <View>
+              <Text>{name}</Text>
+              <Text>{description}</Text>
+            </View>
             <Image
               style={{width:100, height:100}}
               source={{ uri: qrCode }}
@@ -18,6 +20,10 @@ export default function EventItem({id, name, description, qrCode}) {
 
 const styles = StyleSheet.create({
     card: {
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'space-round',
       borderWidth: 1,
       borderColor: '#c5c5c5',
       borderRadius: 10,
